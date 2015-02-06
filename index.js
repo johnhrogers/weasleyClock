62a88a1a55673d0642e949090c440a16e28f4cea
John Rogers <john.rogers@appian.com>
2015-02-05 22:46:43 -0500

    Working query hooray

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 var foo;
-// console.log("1");
-function parseData(error, response, body) {
-	if (!error && response.statusCode == 200) {
-		console.log(JSON.parse(body).feed.entry.length);foo = JSON.parse(body).feed.entry;
-		console.log(foo[foo.length-1]['gsx$location']['$t']);
-	}
-}
-// console.log("2");
-var request = require('request');
-// console.log("3");
-var url = "https://spreadsheets.google.com/feeds/list/1zb2XfqgDrv8pG1hqgigFiMI3oqY4a83B1kPxE5BU9wU/od6/public/values?alt=json";
-// console.log("4");
-request(url,parseData);
+function parseData(res) {
+	console.log("Got response: " + res.statusCode);
+	res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+    // foo = JSON.parse(chunk).feed.entry;
+    // console.log(foo.feed.entry);
+    // console.log(foo[foo.length-1]['gsx$location']['$t']);
+  });
+  res.on('error', function(e) {
+	  console.log("Got error: " + e.message);
+	});
 
+	// if (!error && response.statusCode == 200) {
+	// 	console.log(JSON.parse(body).feed.entry.length);
+	// 	foo = JSON.parse(body).feed.entry;
+	// 	console.log(foo[foo.length-1]['gsx$location']['$t']);
+	// } else {
+ //  	console.log(error)
+ //  }
+};
+console.log("2");
+var tessel = require('tessel');
+var http = require('http');
+var url = "http://spreadsheets.google.com/feeds/list/1zb2XfqgDrv8pG1hqgigFiMI3oqY4a83B1kPxE5BU9wU/od6/public/values?alt=json";
+http.get(url,parseData);
+console.log("5");
+
+// exports.parseData = parseData;
